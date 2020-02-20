4f1d91a4547cbd0777ce181d32d089abe139924e
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-02-19 21:45:34 -0500

    Fixed some of the prompt questions to match manager

--- Develop/app.js
+++ Develop/app.js
@@ -5,19 +5,24 @@ inquirer
   .prompt([
     {
     type: "input",
-    message: "Hello manager, what is your name?",
+    message: "Please enter the name of the manager.",
       name: "name"
     },
     {
       type: "input",
-      message:"And your Id?",
+      message:"Please enter the manager's Id",
       name: "id"
     },
     {
       type: "input",
-      message:"Lastly, your email?",
+      message:"The manager's Email?",
       name: "email"
+    },
+    {
+      type: "input",
+      message: ""
     }
+
 ]).then(function(answer) {
     console.log(answer);
     console.log(answer.id)
